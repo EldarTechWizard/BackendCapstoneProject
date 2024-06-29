9b656f08bb443dbbb9a81c5e872a0bce018b3071
EldarTechWizard <eldargonzalez08@gmail.com>
2024-06-29 16:02:41 -0700

    Added serializers, a view set and registered new routes

--- BackendFinalProject/restaurant/views.py
+++ BackendFinalProject/restaurant/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .serializers import MenuSerializer,BookingSerializer
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet 
+from .models import Menu,Booking
+
+# Create your views here. 
+class BookingViewClass(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
 def index(request):
     return render(request, 'index.html', {})
