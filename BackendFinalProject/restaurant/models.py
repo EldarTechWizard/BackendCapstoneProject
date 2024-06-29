8fb324a9b213cc962704993ec1dfeda6a52ff1ba
EldarTechWizard <eldargonzalez08@gmail.com>
2024-06-29 16:58:03 -0700

    Some fixes to the final version

--- BackendFinalProject/restaurant/models.py
+++ BackendFinalProject/restaurant/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name + " (" + self.email + ")"
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guest = models.IntegerField()
     bookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
